e9a3f73f43193db68425a3339d40e8a882923d68
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2026-08-08 23:57:31 +0530

    Day 39: Built React dashboard with state-driven UI and component layout

--- DAY_38_React_js/src/App.jsx
+++ DAY_38_React_js/src/App.jsx
@@ -1,11 +1,13 @@
-import DAY_38 from './DAY_38_React_Basics/App.jsx'
+// import DAY_38 from './DAY_38_React_Basics/App.jsx';
+import DAY_39 from "./DAY_39_EventHandling_ConditionalRendering/App.jsx";
 
 
 function App() {
 
   return (
     <>
-    <DAY_38 />
+    {/* <DAY_38 /> */}
+    <DAY_39 />
     </>
   )
 }
